1fc3b1028663eade3079117e846de1863bc85ece
토니 <mightytonykr@gmail.com>
2023-12-29 01:01:00 +0900

    Initial commit

---
+++ src/main/java/mightytony/sideproject/dayoffmanager/DayOffManagerApplication.java
@@ -0,0 +1,13 @@
+package mightytony.sideproject.dayoffmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayOffManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayOffManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mightytony/sideproject/dayoffmanager/DayOffManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package mightytony.sideproject.dayoffmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayOffManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
