bc2c5cb36f914592005af7e91e27410521bc7a97
tony <mightytonykr@gmail.com>
2024-01-03 16:32:29 +0900

    JwtTokenProvider, JwtAuthenticationFilter 구현

---
+++ src/main/java/mightytony/sideproject/dayoffmanager/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package mightytony.sideproject.dayoffmanager.config.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        // 1. Request Header에서 JWT 토큰 추출
+        String token = resolveToken((HttpServletRequest) request);
+
+        // 2. validateToken으로 토큰 유효성 검사
+        // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext에 저장
+        if(token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        // 3. 다음 필터로 이동
+        filterChain.doFilter(request,response);
+    }
+
+    // Request Header에서 토큰 정보 추출
+    private String resolveToken(HttpServletRequest request) {
+        // 1. 헤더 중 Authorization 헤더를 가져 옴
+        String bearerToken = request.getHeader("Authorization");
+
+        // 2. Authorization 헤더 value 가 Bearer로 시작 한다면 그 뒤 값 반환.
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    /**
+     * @apiNote doFilter()
+     * 1. resolveToken() 메서드를 사용하여 요청 헤더에서 JWT 토큰을 추출
+     * 2. JwtTokenProvider의 validateToken() 메서드로 JWT 토큰 유효성 검증
+     * 3. 토큰이 유효하면 JwtTokenProvider의 getAuthentication() 메서드로 인증 객체 가져와서 SecurityContext에 저장
+     * => 요청을 처리하는 동안 인증 정보가 유지된다.
+     * 4. filterChain.doFilter() 를 호출하여 다음 필터로 요청 전달
+     *
+     * @apiNote resolveToken()
+     * 1. 주어진 HttpServletRequest에서 토큰 정보를 추출하는 역할
+     * 2. "Authorization" 헤더에서 "Bearer" 접두사로 시작하는 토큰을 추출하여 반환
+     */
+}
--- src/main/java/mightytony/sideproject/dayoffmanager/config/jwt/JwtTokenProvider.java
+++ src/main/java/mightytony/sideproject/dayoffmanager/config/jwt/JwtTokenProvider.java
@@ -35,7 +35,11 @@ public class JwtTokenProvider {
         this.key = Keys.hmacShaKeyFor(keyBytes);
     }
 
-    // User의 정보를 가지고 AccessToken, RefreshToken을 생성하는 메서드
+    /**
+     * User의 정보를 가지고 AccessToken, RefreshToken을 생성하는 메서드
+     * Access Token : 인증된 사용자의 권한 정보와 만료 시간을 담고 있음
+     * Refresh Token : Access Token 의 갱신을 위해 사용 됨
+     */
     public JwtToken generateToken(Authentication authentication) {
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
@@ -64,7 +68,16 @@ public class JwtTokenProvider {
                 .build();
     }
 
-    // Jwt 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드
+    /**
+     * @apiNote : Jwt 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드
+     * 주어진 Access Token을 복호화하여 사용자의 인증 정보(Authentication)를 생성
+     * 토큰 안의 Claims에서 권한 정보를 추출, User 객체를 생성하여 Authentication 객체로 변환
+     * 과정
+     * 1. Claims에서 권한 정보를 가져옴. 'auth' 클레임은 토큰에 저장된 권한 정보를 나타 냄
+     * 2. 가져온 권한 정보를 SimpleGrantedAuthority 객체로 변환하여 컬렉션에 추가
+     * 3. UserDetails 객체를 생성하여 주체, 권한 정보, 기타 필요 정보를 설정
+     * 4. UsernamepasswordAuthenticationToken 객체를 생성해 주체와 권한 정보를 포함한 인증(Authentication) 객체를 생성
+     */
     public Authentication getAuthentication(String accessToken) {
         // Jwt 토큰 복호화
         Claims claims = parseClaims(accessToken);
@@ -72,6 +85,7 @@ public class JwtTokenProvider {
         if(claims.get("auth") == null) {
             // fixme : 예외처리 하드하게 되어있음.
             throw new RuntimeException("권한 정보가 없는 토큰 입니다.");
+
         }
 
         // 클레임에서 권한 정보 가져오기
@@ -87,6 +101,11 @@ public class JwtTokenProvider {
     }
 
     // 토큰 정보를 검증하는 메서드
+    /**
+     * 클레임(Claims) : 토큰에서 사용할 정보의 조각
+     * 주어진 Access Token을 복호화하고, 만료된 토큰일 경우도 Claims 반환
+     * parseClaimsJws() 메서드가 JWT 토큰의 검증,파싱을 함.
+     */
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder()
@@ -119,4 +138,5 @@ public class JwtTokenProvider {
         }
     }
 
+    // JWT를 통해 username + password 인증 수행.
 }
