798b727471f1ba6897d20e9e151b4bf08dcf1aee
tndus2 <j1m3p2@naver.com>
2024-01-14 23:07:02 +0900

    column 타입 길이 설정 추가

--- src/main/java/mightytony/sideproject/dayoffmanager/company/domain/Company.java
+++ src/main/java/mightytony/sideproject/dayoffmanager/company/domain/Company.java
@@ -10,18 +10,18 @@ import org.hibernate.annotations.Comment;
 /**
  * 회사 테이블
  */
-@Entity
-@Builder
+@Entity // jpa가 여기 작성된것을 테이블로 인식할 수 있도록 선언
+@Builder  // builder 패턴
 @NoArgsConstructor
 @AllArgsConstructor
 public class Company extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Comment("고유 번호")
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // @Id : pk 설정, @GeneratedValue : 생성전략설정
+    @Comment("고유 번호") // 컬럼 코멘트
     private Long id;
 
     @Comment("사업자등록번호")
-    @Column(name = "b_no",nullable = false, unique = true)
+    @Column(name = "b_no",nullable = false, unique = true, length = 10) // length : 컬럽 타입 길이 설정
     private String businessNumber;
 
     @Comment("개업일자 (YYYYMMDD 포맷)")
