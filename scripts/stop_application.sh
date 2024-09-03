89d66ea226bd7002bb60b4ce99e8827c33d016f7
토니 <mightytonykr@gmail.com>
2024-09-04 01:18:06 +0900

    스크립트 수정 6

--- scripts/stop_application.sh
+++
@@ -1,8 +0,0 @@
-##!/bin/bash
-#set -e
-#
-## Docker 컨테이너 중지 및 제거
-#echo "Attempting to stop all containers using docker-compose.yml located at /home/ec2-user/app/"
-#docker-compose -f /home/ec2-user/app/docker-compose.yml down
-#
-#echo "Docker containers stopped successfully."
