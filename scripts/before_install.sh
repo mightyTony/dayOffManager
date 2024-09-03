c2e18ea825140197881c1e2aebdc8725ad379907
토니 <mightytonykr@gmail.com>
2024-09-04 02:32:54 +0900

    스크립트 수정 11

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-set -e
-
-# 필요한 도구 설치
-sudo yum update -y
-sudo yum install -y docker
-sudo systemctl start docker
-sudo systemctl enable docker
+#set -e
+#
+## 필요한 도구 설치
+#sudo yum update -y
+#sudo yum install -y docker
+#sudo systemctl start docker
+#sudo systemctl enable docker
+#
+## Docker Compose 설치
+#if [ ! -f /usr/local/bin/docker-compose ]; then
+#    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#    sudo chmod +x /usr/local/bin/docker-compose
+#fi
 
 # 이전 배포 파일 삭제
 if [ -d "/home/ec2-user/app" ]; then
   sudo rm -rf /home/ec2-user/app/*
-fi
-
-# Docker Compose 설치
-if [ ! -f /usr/local/bin/docker-compose ]; then
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 # Docker 컨테이너 정지 및 네트워크, 볼륨 제거
