080593683e7bcf6d5d215c072eddf9126bd44422
토니 <mightytonykr@gmail.com>
2024-09-20 16:46:16 +0900

    커밋 꼬인거 수정

--- src/main/java/mightytony/sideproject/dayoffmanager/controller/TestController.java
+++
@@ -1,7 +0,0 @@
-package mightytony.sideproject.dayoffmanager.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TestController {
-}
--- src/main/java/mightytony/sideproject/dayoffmanager/service/MemberService.java
+++
@@ -1,8 +0,0 @@
-package mightytony.sideproject.dayoffmanager.service;
-
-import mightytony.sideproject.dayoffmanager.config.jwt.JwtToken;
-
-public interface MemberService {
-
-    public JwtToken signIn(String username, String password);
-}
